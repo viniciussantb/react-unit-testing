f788318c05f2fe1071f42d9b82535b58a96ec3de
Vinícius Santiago <55957552+viniciussantb@users.noreply.github.com>
2023-03-28 20:17:36 -0300

    button tests

--- react-unit-testing/src/App.spec.tsx
+++ react-unit-testing/src/App.spec.tsx
@@ -1,9 +1,19 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('', () => {
-    const { getByText } = render(<App />);
+describe('Testing App component', () => {
+    it('should have Hello World', () => {
+        const { getByText } = render(<App />);
+        expect(getByText('Hello World')).toBeInTheDocument();
+    });
 
-    expect(getByText('Hello World')).toBeInTheDocument();
-    expect(getByText('Hello World')).toHaveAttribute('class', 'test');
+    it('should add user Vinícius', async () => {
+        const { getByText } = render(<App />);
+        await userEvent.click(getByText('Add Name'));
+
+        expect(getByText('Patrick')).toBeInTheDocument();
+        expect(getByText('Vinícius')).toBeInTheDocument();
+        expect(getByText('Add Name')).toBeDisabled();
+    });
 });
--- react-unit-testing/src/App.tsx
+++ react-unit-testing/src/App.tsx
@@ -1,8 +1,29 @@
 import { useState } from 'react';
 
 function App() {
+  const [names, setNames] = useState([
+    'Arthur',
+    'Erick',
+    'Patrick',
+    'Victor'
+  ]);
+  const [disable, setDisable] = useState(false);
+
+  function handleClick() {
+    setNames((state) => [...state, 'Vinícius']);
+    setDisable(true);
+  }
+
+  const listNames = names.map(name => <li key={name}>{name}</li>)
+
   return (
-    <h1 className='test'>Hello World</h1>
+    <>
+      <h1 className='test'>Hello World</h1>
+      <button disabled={disable} className='AddName' onClick={handleClick}>Add Name</button>
+      <ul>
+        {listNames}
+      </ul>
+    </>
   )
 }
 
